750f61928ae07d59911c684b664a063e2a898091
Starvader <ashutoshgupta1311@gmail.com>
2026-03-22 13:22:03 +0000

    Update check sum
    Signed-off-by: Starvader <ashutoshgupta1311@gmail.com>

--- Formula/bridgellm.rb
+++ Formula/bridgellm.rb
@@ -2,7 +2,7 @@ class Bridgellm < Formula
   desc "Let your AI coding agents talk to each other across services"
   homepage "https://github.com/starvader13/bridgellm"
   url "https://registry.npmjs.org/bridgellm/-/bridgellm-0.1.7.tgz"
-  sha256 "c8d3eae160a892e32837db3dcae515e843e5383fef52b8141940c8bcf8b6d59f"
+  sha256 "fcef76624f87ff611cc572ea5ff3ee1bae86e202e39725b731674f4e206c5200"
   license "MIT"
 
   depends_on "node"
